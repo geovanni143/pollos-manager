4f2c57269e1c1a47ecbf70cadec346e4e3d26956
KahlidL <kahlidliconar@micorreo.upp.edu.mx>
2025-03-17 15:37:18 -0600

    Revisión de funcionamiento

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Productos from "./pages/Productos";
-import Ventas from "./pages/Ventas"; // 🔹 Importamos la nueva página de ventas
+import Ventas from "./pages/Ventas"; // 🔹 Se importa la nueva página de ventas
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/productos" element={<Productos />} />
-                <Route path="/ventas" element={<Ventas />} /> {/* 🔹 Nueva ruta para ver las ventas */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/ventas" element={<Ventas />} />{" "}
+        {/* 🔹 Nueva ruta para ver las ventas */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
